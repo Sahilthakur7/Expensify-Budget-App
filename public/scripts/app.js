0a80bae362cd79cdd0b05d120222b73fe7c7bf94
SahilThakur7 <sahilrd07@gmail.com>
2018-09-29 09:39:13 +0530

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,78 @@
 'use strict';
 
+var app = {
+    title: 'Indecision App',
+    subtitle: "Lets you decide",
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  'p',
-  null,
-  'ThiS is SPARTA!!!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? "Here are the options" : 'No Options For you'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var user = {
+    name: 'Sahil Thakur',
+    age: 22,
+    location: 'Shimla'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location,
+            ' '
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,5 +1,42 @@
+var app = {
+    title: 'Indecision App',
+    subtitle: "Lets you decide",
+    options: ['One', 'Two']
+}
 
-var template = <p>ThiS is SPARTA!!!</p>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {(app.subtitle) && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are the options" : 'No Options For you'}</p>
+        <ol>
+            <li>Item one</li> 
+            <li>Item two</li> 
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Sahil Thakur',
+    age: 22,
+    location: 'Shimla'
+};
+
+function getLocation(location){
+    if(location){
+        return (
+            <p>Location: {location} </p>
+        );
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
