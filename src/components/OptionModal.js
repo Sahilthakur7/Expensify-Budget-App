ec7a126d0508ca3c5ba3ec6463e91b2bd33add86
SahilThakur7 <sahilrd07@gmail.com>
2018-11-26 10:22:28 +0530

    modal styled

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -8,10 +8,14 @@ const OptionModal = (props) => {
          isOpen = {!!props.selectedOption} 
          onRequestClose = {props.handleRemoveSelectedOption}
          contentLabel='Selected Option'
+         closeTimeoutMS={200}
+         className="modal"
         >
-            <h3>Selected Option</h3>
-            <p>{props.selectedOption}</p>
-            <button onClick={props.handleRemoveSelectedOption}>Thank You</button>
+            <h3 className="modal__title">Selected Option</h3>
+            <p className="modal__body">{props.selectedOption}</p>
+            <button 
+                className="button"
+                onClick={props.handleRemoveSelectedOption}>Thank You</button>
         </Modal>
     );
 }
