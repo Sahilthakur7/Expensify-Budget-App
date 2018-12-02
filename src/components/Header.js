0e6a625d4574a3e61e4816cab4b1cfede65a51a4
SahilThakur7 <sahilrd07@gmail.com>
2018-12-02 11:48:46 +0530

    store created and fiddled with

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/Edit" activeClassName="is-active">Edit</NavLink>
         <NavLink to="/Help" activeClassName="is-active">Help</NavLink>
     </header>
 );
