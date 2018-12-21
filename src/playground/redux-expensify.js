84cb41460cc3a79891169267e63a097b8d4e08c2
SahilThakur7 <sahilrd07@gmail.com>
2018-12-21 11:18:02 +0530

    edit expense left, battery finished

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,83 @@
 import { createStore , combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//ADD_EXPENSE
+
+const addExpense = ( { description='', note='', amount = 0, createdAt =0} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
+// REMOVE_EXPENSE
+
+const removeExpense = ( {id} = {}) => (
+    {
+        type: 'REMOVE_EXPENSE',
+        id
+    }
+)
+
+// EDIT_EXPENSE
+
+const editExpense = ( {id,update} = {}) => ( {
+    type: 'EDIT_EXPENSE',
+    id,
+})
+
+const expensesReducerDefaultState = [];
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const expensesReducer = (state = expensesReducerDefaultState,action) => {
+    switch(action.type){
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id}) => {
+                return id !== action.id;
+            });
+
+        default:
+            return state;
+    }
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+const store = createStore(
+    combineReducers(
+        {
+            expenses: expensesReducer,
+            filters: filtersReducer
+        }
+    )
+);
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne =store.dispatch(addExpense({ description: 'Rent', amount: 100}));
+// store.dispatch(removeExpense( { id: expenseOne.expense.id}));
+store.dispatch(editExpense(expenseOne.expense.id, { amount: 500}));
 
 const demoState = {
     expenses: [{
