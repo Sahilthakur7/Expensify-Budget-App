65ed64d101da86be1470b2c38b4ec829bd85c1d7
SahilThakur7 <sahilrd07@gmail.com>
2019-01-15 10:23:59 +0530

    selectors tested

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -29,3 +29,17 @@ test('should generate set text filter object with text value', () => {
         text
     });
 })
+
+
+test('should generate set text filter object with default', () => {
+    const action = setTextFilter();
+
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    });
+});
+
+// test('should generate action object for sort by date', () => {
+//     expect(sortByDate()).toEqual({type: 'SORT_BY_DATE'});
+// });
