b6db1559b9ef63ea5ac8ae5ad7328c3997346c8c
SahilThakur7 <sahilrd07@gmail.com>
2019-01-24 11:23:21 +0530

    created firebase database and connected to it

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const config = {
+        apiKey: "AIzaSyAVRINb1KfXEgkTKl6lT89Ja89tnTuXMhw",
+        authDomain: "expensify-35cd4.firebaseapp.com",
+        databaseURL: "https://expensify-35cd4.firebaseio.com",
+        projectId: "expensify-35cd4",
+        storageBucket: "expensify-35cd4.appspot.com",
+        messagingSenderId: "347921571148"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+    name: 'Sahil Thakur'
+});
